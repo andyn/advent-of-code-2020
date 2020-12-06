9cfc264c97cc13f81c6792af16ccfa1226a0c6bb
Antti Nilakari <antti.nilakari@iki.fi>
2020-12-06 02:12:28 +0200

    Day 4 star 2

--- day4/day4.exs
+++ day4/day4.exs
@@ -7,10 +7,16 @@ defmodule Day3 do
     passports = read_passports(handle)
 
     star1_valid_passports =
-      (for passport <- passports, do: passport_valid? passport)
+      (for passport <- passports, do: naive_passport_valid? passport)
       |> Enum.filter(fn x -> x == true end)
       |> Enum.count()
     IO.puts("Star 1: #{star1_valid_passports} valid passports.")
+
+    star2_valid_passports =
+      (for passport <- passports, do: strict_passport_valid? passport)
+      |> Enum.filter(fn x -> x == true end)
+      |> Enum.count()
+    IO.puts("Star 2: #{star2_valid_passports} valid passports.")
 
   end
 
@@ -27,7 +33,7 @@ defmodule Day3 do
     end
   end
 
-  defp passport_valid?(passport) do
+  defp naive_passport_valid?(passport) do
     fields = [:byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid, :cid]
     found_fields = for field <- fields, do: Map.has_key?(passport, field)
     case found_fields |> Enum.filter(fn x -> x == true end) |> Enum.count() do
@@ -37,6 +43,77 @@ defmodule Day3 do
     end
   end
 
+  defp strict_passport_valid?(passport) do
+    birth_year_valid? = case Map.get(passport, :byr) do
+      nil ->
+        false
+      something -> case Integer.parse(something) do
+        {birth_year, ""} when birth_year in 1920..2002 -> true
+        _ -> false
+      end
+    end
+
+    issue_year_valid? = case Map.get(passport, :iyr) do
+      nil ->
+        false
+      something -> case Integer.parse(something) do
+        {issue_year, ""} when issue_year in 2010..2020 -> true
+        _ -> false
+      end
+    end
+
+    expiration_year_valid? = case Map.get(passport, :eyr) do
+      nil ->
+        false
+      something -> case Integer.parse(something) do
+        {expiration_year, ""} when expiration_year in 2020..2030 -> true
+        _ -> false
+      end
+    end
+
+    height_valid? = case Map.get(passport, :hgt) do
+      nil ->
+        false
+      something -> case Integer.parse(something) do
+        {height_cm, "cm"} when height_cm in 150..193 -> true
+        {height_in, "in"} when height_in in 59..76 -> true
+        _ -> false
+      end
+    end
+
+    hair_color_valid? = case Map.get(passport, :hcl) do
+      nil -> false
+      something -> String.match?(something, ~r/^#[0-9a-f]{6}$/)
+    end
+
+    eye_color_valid? = case Map.get(passport, :ecl) do
+      "amb" -> true
+      "blu" -> true
+      "brn" -> true
+      "gry" -> true
+      "grn" -> true
+      "hzl" -> true
+      "oth" -> true
+      _ -> false
+    end
+
+    passport_id_valid? = case Map.get(passport, :pid) do
+      nil -> false
+      something -> something |> String.match?(~r/^[0-9]{9}$/)
+    end
+
+    unless (birth_year_valid?
+      and issue_year_valid?
+      and expiration_year_valid?
+      and height_valid?
+      and hair_color_valid?
+      and eye_color_valid?
+      and passport_id_valid?) do
+      :false
+    else
+      :true
+    end
+  end
 end
 
 Day3.main(System.argv())
